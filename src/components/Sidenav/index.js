02b718dc60b0e4309da8655f540ed340b9cd56f4
klepa <pedro.klepa@gmail.com>
2020-09-03 16:06:43 -0300

    Fixed sidenav for vertical scrolling

--- src/components/Sidenav/index.js
+++ src/components/Sidenav/index.js
@@ -5,57 +5,59 @@ import "./index.css";
 function Sidenav() {
   return (
     <div className="sidenav">
-      <header>
-        <h1>
-          sci-fi<strong>nd</strong>
-        </h1>
-      </header>
+      <div className="sidenav-content">
+        <header>
+          <h1>
+            sci-fi<strong>nd</strong>
+          </h1>
+        </header>
 
-      <section>
-        <h2>Easy</h2>
+        <section>
+          <h2>Easy</h2>
 
-        <div className="character">
-          <p>Data</p>
-          <span>Star Trek: Next Generation (1987 ~ 1994)</span>
-        </div>
+          <div className="character">
+            <p>Data</p>
+            <span>Star Trek: Next Generation (1987 ~ 1994)</span>
+          </div>
 
-        <div className="character">
-          <p>John Connor</p>
-          <span>The Terminator (1984)</span>
-        </div>
-      </section>
+          <div className="character">
+            <p>John Connor</p>
+            <span>The Terminator (1984)</span>
+          </div>
+        </section>
 
-      <hr />
+        <hr />
 
-      <section>
-        <h2>Medium</h2>
+        <section>
+          <h2>Medium</h2>
 
-        <div className="character">
-          <p>Agent Smith</p>
-          <span>The Matrix (1999)</span>
-        </div>
+          <div className="character">
+            <p>Agent Smith</p>
+            <span>The Matrix (1999)</span>
+          </div>
 
-        <div className="character">
-          <p>R2-D2</p>
-          <span>Star Wars (1977)</span>
-        </div>
-      </section>
+          <div className="character">
+            <p>R2-D2</p>
+            <span>Star Wars (1977)</span>
+          </div>
+        </section>
 
-      <hr />
+        <hr />
 
-      <section>
-        <h2>Hard</h2>
+        <section>
+          <h2>Hard</h2>
 
-        <div className="character">
-          <p>Astroboy</p>
-          <span>Astroboy (2009)</span>
-        </div>
+          <div className="character">
+            <p>Astroboy</p>
+            <span>Astroboy (2009)</span>
+          </div>
 
-        <div className="character">
-          <p>Bender</p>
-          <span>Futurama (1999 ~ 2013)</span>
-        </div>
-      </section>
+          <div className="character">
+            <p>Bender</p>
+            <span>Futurama (1999 ~ 2013)</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
