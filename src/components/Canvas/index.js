9b1902466704a78b8a0806713d137cf507e7b496
klepa <pedro.klepa@gmail.com>
2020-09-02 21:33:33 -0300

    Add some TODO comments

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -5,6 +5,7 @@ import './index.css'
 import SelectionArea from '../SelectionArea'
 
 function Canvas() {
+  // TODO: It is preferred to have the start location of the circle to be the center of the image
   const [selectionX, setSelectionX] = useState(800)
   const [selectionY, setSelectionY] = useState(400)
   const [showSelection, setShowSelection] = useState(false)
@@ -12,6 +13,7 @@ function Canvas() {
   function handleClick(e) {
     if (!showSelection) setShowSelection(true)
 
+    // Offset the click location in half the diameter of the SelectionArea circle in order to properly center itself
     setSelectionX(e.pageX - 50)
     setSelectionY(e.pageY - 50)
   }
@@ -19,6 +21,8 @@ function Canvas() {
   return (
     <div className="canvas-container" onClick={handleClick}>
       <SelectionArea showSelection={showSelection} x={selectionX} y={selectionY} />
+
+      // TODO: Add Waldo Image
     </div>
   )
 }
