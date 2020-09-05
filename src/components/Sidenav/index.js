2a542208512e4f57153e457082bf4ce105c73258
klepa <pedro.klepa@gmail.com>
2020-09-05 16:18:51 -0300

    Sidebar now links to each character IMDb page

--- src/components/Sidenav/index.js
+++ src/components/Sidenav/index.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import "./index.css";
 
-// TODO: Link each character to its IMDb page with a photo for reference
-
 // TODO: Cross character name when found
 
 // TODO: Instead of hardcoding characters, map from the respective .json file
@@ -21,14 +19,30 @@ function Sidenav() {
         <section>
           <h2>Easy</h2>
 
-          <div className="character">
+          <div
+            className="character"
+            onClick={() =>
+              window.open(
+                "https://www.imdb.com/title/tt0092455/characters/nm0000653?ref_=tt_cl_t5",
+                "_blank"
+              )
+            }
+          >
             <p>Data</p>
             <span>Star Trek: Next Generation (1987 ~ 1994)</span>
           </div>
 
-          <div className="character">
+          <div
+            className="character"
+            onClick={() =>
+              window.open(
+                "https://www.imdb.com/title/tt0103064/characters/nm0000411?ref_=tt_cl_t3",
+                "_blank"
+              )
+            }
+          >
             <p>John Connor</p>
-            <span>The Terminator (1984)</span>
+            <span>The Terminator 2 (1991)</span>
           </div>
         </section>
 
@@ -37,12 +51,28 @@ function Sidenav() {
         <section>
           <h2>Medium</h2>
 
-          <div className="character">
+          <div
+            className="character"
+            onClick={() =>
+              window.open(
+                "https://www.imdb.com/title/tt0133093/characters/nm0915989?ref_=tt_cl_t4",
+                "_blank"
+              )
+            }
+          >
             <p>Agent Smith</p>
             <span>The Matrix (1999)</span>
           </div>
 
-          <div className="character">
+          <div
+            className="character"
+            onClick={() =>
+              window.open(
+                "https://www.imdb.com/title/tt0076759/characters/nm0048652?ref_=tt_cl_t7",
+                "_blank"
+              )
+            }
+          >
             <p>R2-D2</p>
             <span>Star Wars (1977)</span>
           </div>
@@ -53,12 +83,28 @@ function Sidenav() {
         <section>
           <h2>Hard</h2>
 
-          <div className="character">
+          <div
+            className="character"
+            onClick={() =>
+              window.open(
+                "https://www.imdb.com/title/tt0375568/characters/nm0383603?ref_=tt_cl_t2",
+                "_blank"
+              )
+            }
+          >
             <p>Astroboy</p>
             <span>Astroboy (2009)</span>
           </div>
 
-          <div className="character">
+          <div
+            className="character"
+            onClick={() =>
+              window.open(
+                "https://www.imdb.com/title/tt0149460/characters/nm0224007?ref_=tt_cl_t2",
+                "_blank"
+              )
+            }
+          >
             <p>Bender</p>
             <span>Futurama (1999 ~ 2013)</span>
           </div>
